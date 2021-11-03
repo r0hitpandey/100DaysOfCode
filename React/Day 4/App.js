2d3933f90b270c00185fcf399178584dcf10d7aa
rohit pandey <68698939+r0hitpandey@users.noreply.github.com>
2021-11-03 09:34:42 +0530

    Add files via upload

---
+++ React/Day 4/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+
+function App() {
+  return (
+    // this portion is JSX
+    <>
+      <Navbar title="TextUtils" AboutText="About us" />
+    </>
+  );
+}
+
+export default App;
